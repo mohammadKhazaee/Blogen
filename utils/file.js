b072b697ce31cac7a56035c2b336cd04ce8cdad3
mohammad <m.khazaee.p@gmail.com>
2023-10-02 17:55:40 +0330

    delete image path fixed

--- utils/file.js
+++ utils/file.js
@@ -1,10 +1,12 @@
-const fs = require('fs')
+const fs = require("fs");
 
-exports.deleteFile = path => {
-	path = path.substring(1)
-	path = path.replace('/', '\\')
-	fs.unlink(path, err => {
-		if (err) throw err
-		console.log('*** image deleted ***')
-	})
-}
+exports.deleteFile = (path) => {
+	path = path.substring(1);
+	if (process.platform === "win32") {
+		path = path.replace("/", "\\");
+	}
+	fs.unlink(path, (err) => {
+		if (err) console.log(err);
+		console.log("*** image deleted ***");
+	});
+};
